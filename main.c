09c7942efea588ab75d7a57c2500f0c7ab7f4726
SimonBerghem <48282727+SimonBerghem@users.noreply.github.com>
2020-01-31 11:52:44 +0100

    Init

---
+++ main.c
@@ -0,0 +1,22 @@
+/*
+ * GccApplication1.c
+ *
+ * Created: 2020-01-31 11:20:49
+ * Author : Erik Serrander, Simon Malmström Berghem
+ */ 
+
+#include <avr/io.h>
+#include "mytest.c"
+#include "tinythreads.c"
+#include "tinythreads.h"
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+		int x = 2;
+    }
+}
+
